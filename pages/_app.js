69dc07b8b0f1da1d38402aae66a09c59b6eb1a9f
toshinori8 <zbigniew.karski@gmail.com>
2021-03-31 14:17:29 +0200

    nn

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,26 @@ import "../styles/box_ofer.css";
 import "tailwindcss/tailwind.css";
 import Layout from "../components/layout";
 import { AnimateSharedLayout } from "framer-motion";
+import React from "react";
 
-// Store Strapi Global object in context
-// export const GlobalContext = createContext({});
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, preloadTrue }) {
+ 
+  React.useEffect(() => {
+
+   
+    alert('wgralem strone');
+    
+  }, [])
   return (
-    <Layout>
+ 
+ 
+ <Layout>
       <AnimateSharedLayout>
         <Component {...pageProps} />
       </AnimateSharedLayout>
     </Layout>
   );
+
+
 }
