338af6b90fd00a9919494de2d21cd14b2f691055
toshinori8 <zbigniew.karski@gmail.com>
2021-04-25 20:57:27 +0200

    xcļcxv

--- pages/test.js
+++ pages/test.js
@@ -8,17 +8,19 @@ export default function Test() {
   return (
     <>
       <motion.div
-        className="anim-oferta-image w-1/2  mt-10 text-white bg-gray-500 m-20"
-        animate={{ perspective: 30, opacity: 1, skewX: "0deg", skewY: "10deg" }}
+        className="anim-oferta-image w-1/2 h-full  bg-gray-500"
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.3, delay: 3 }}
         initial={{ opacity: 0, skewY: "0deg" }}
       >
-        <div key="id0" className="flex-box-mask">
-          <div key="id2" className="flex-box-mask-item">x</div>
-          <div key="id3" className="flex-box-mask-item">x</div>
-          <div key="id4" className="flex-box-mask-item">x</div>
-          <div key="id5" className="flex-box-mask-item">x</div>
+        <div key="id0" className="flex-box-mask opacity-40 absolute">
+          <motion.div initial={{  }} key="id2" className="flex-box-mask-item h-full p-0 m-0 opacity-20"></motion.div>
+          <div key="id3" className="flex-box-mask-item h-3/6 absolute"></div>
+          <div key="id4" className="flex-box-mask-item h-full"></div>
+          <div key="id5" className="flex-box-mask-item h-full"></div>
         </div>
+
+        <img className="image_flex" src="http://stream404.art.pl/wp-content/uploads/2021/02/elektrolabs.jpg"/>
       </motion.div>
     </>
   );
