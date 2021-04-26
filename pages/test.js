7ab0f6f8ef0585b641176d08c0d9f2d44b44f5ff
toshinori8 <zbigniew.karski@gmail.com>
2021-04-26 08:31:42 +0200

    asd

--- pages/test.js
+++ pages/test.js
@@ -7,20 +7,34 @@ import MetaTags from "react-meta-tags";
 export default function Test() {
   return (
     <>
+    
       <motion.div
-        className="anim-oferta-image w-1/2 h-full  bg-gray-500"
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3, delay: 3 }}
+        className="anim-oferta-image w-1/2 h-full  bg-gray-500 transparent"
+        animate={{ opacity: 1}}
         initial={{ opacity: 0, skewY: "0deg" }}
       >
-        <div key="id0" className="flex-box-mask opacity-40 absolute">
-          <motion.div initial={{  }} key="id2" className="flex-box-mask-item h-full p-0 m-0 opacity-20"></motion.div>
-          <div key="id3" className="flex-box-mask-item h-3/6 absolute"></div>
-          <div key="id4" className="flex-box-mask-item h-full"></div>
-          <div key="id5" className="flex-box-mask-item h-full"></div>
+        <img
+          className="image_flex"
+          src="http://stream404.art.pl/wp-content/uploads/2020/06/bilbielsko.jpg"
+        />
+
+        <div key="id0" className="flex-box-mask  absolute">
+          <motion.div
+            initial={{ height:12 }}
+            animate={{ height: 100, delay:100 }}
+            key="id1"
+            className="flex-box-mask-item"
+          ></motion.div>
+          
+         
+          <div key="id3" className="flex-box-mask-item"></div>
+          <div key="id3" className="flex-box-mask-item"></div>
+          <div key="id4" className="flex-box-mask-item"></div>
+          <div key="id5" className="flex-box-mask-item"></div>
         </div>
 
-        <img className="image_flex" src="http://stream404.art.pl/wp-content/uploads/2021/02/elektrolabs.jpg"/>
+        {/* <img className="image_flex" src="http://stream404.art.pl/wp-content/uploads/2021/02/elektrolabs.jpg"/> */}
+        
       </motion.div>
     </>
   );
