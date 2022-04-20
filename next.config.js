64eedc9f597e30b51f0277cad54af7ec1908e50b
toshinori8 <zbigniew.karski@gmail.com>
2022-04-20 23:31:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,10 +11,11 @@ const withSvgr = require('next-plugin-svgr');
 // module.exports = withSvgr();
 module.exports = withSvgr({
   webpack: function (config, options) {
-    console.log(options.webpack); // 4.44.1
+    //console.log(options.webpack); // 4.44.1
     options.webpack.node = { "fs": "empty" }
     return config;
   },
+  swcMinify:false,
   images: {
     domains: ['strapi.stream404.art.pl'],
   },
