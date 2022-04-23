e85b04970e35404894d3b2a2f38a0ca9a11c1c91
toshinori8 <zbigniew.karski@gmail.com>
2022-04-24 01:44:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,4 +19,18 @@ module.exports = withSvgr({
   images: {
     domains: ['strapi.stream404.art.pl'],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        // source: "/api/:path*",
+        headers: [
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "//strapi.stream404.art.pl" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
 });
