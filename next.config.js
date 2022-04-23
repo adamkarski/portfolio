679354ffbd4ce5d861ea8006efd0910c2e632616
toshinori8 <zbigniew.karski@gmail.com>
2022-04-24 01:47:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ module.exports = withSvgr({
     return [
       {
         // matching all API routes
-        source: "/realizacje/*",
+        source: "/realizacje/[slug]",
         headers: [
           // { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "//strapi.stream404.art.pl" },
