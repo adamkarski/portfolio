4277d4b4b13385e4921f52a7110a80f4079b9e3f
Adam Karski <zbigniew.karski@gmail.com>
2023-07-15 22:39:55 +0200

    Update realizacjeBox.svelte

--- src/lib/components/realizacjeBox.svelte
+++ src/lib/components/realizacjeBox.svelte
@@ -1,94 +1,70 @@
 <script>
-import { fade } from 'svelte/transition';
-import { fly } from 'svelte/transition'
-export let item = {};
+	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
+	export let item = {};
+	export let src = '//strapi.adamkarski.art/' + item.miniatura.url;
 
-let src="";
+	import moreinfo from '$lib/images/moreinfo.svg';
 
-
-import moreinfo from '$lib/images/moreinfo.svg';
-
-function preload(src) {
-	return new Promise(function(resolve) {
-		let img = new Image()
-		img.onload = resolve
-		img.src = src
-	})
+	function preload(src) {
+		return new Promise(function (resolve) {
+			let img = new Image();
+			img.onload = resolve;
+			img.src = src;
+		});
 	}
-
-
-
 </script>
-	
 
-    <div class="w-full p-2 rounded lg:w-1/2 md:w-full xl:w-1/3 item" transition:fade>
+<div class="w-full p-2 rounded lg:w-1/2 md:w-full xl:w-1/3 item" in:fade out:fly>
+	<div style="transform: translateY(150px) translateZ(0px);">
+		<!-- <div style="display: none;">{src="//strapi.adamkarski.art/"+item.miniatura.url}</div> -->
 
-		<div style="transform: translateY(150px) translateZ(0px);">
-			<div style="display: none;">{src = "//strapi.adamkarski.art/"}{src=src+item.miniatura.url}</div>
-		
-			<div class="coverImage">
-			
+		<div class="coverImage">
 			{#await preload(src) then _}
-			<a class="" href="/realizacje/{item.slug}">
-			<img {src} in:fly alt="{item.title}">
-		</a>
-		  {/await}	
-		
-			</div>
+				<a class="" href="/realizacje/{item.slug}">
+					<img {src} in:fly alt={item.title} />
+				</a>
+			{/await}
+		</div>
 
-				<div class=" box_ofer shadow-xl rounded-xl">
-					<div class=" content rounded-xl">
-						
-						<div class=" backgr rounded-md" />
-						<div class=" texts rounded-md">
-							<div class=" p-3 title">
-								<h3 class=" font-semibold text-xl leading-6 my-2">{item.title}</h3>
-								<p class=" paragraph-normal opis">
-									{item.subtitle}
-								</p>
-							</div>
-						</div>
-						<div class=" flex items-center icons">
-							
-							<div class="button_">
-								<a href="/realizacje/{item.slug}">
-										<button
-											class="bg-white text-black px-1 py-2 m-2 rounded-md pl-3"
-											style="transform: none;"
-										>
-										
-										<img alt=""src={moreinfo} class="w10 h10" />
-										
-										</button>
-								</a>
-							</div>
-							
-							<ul class=" list-none flex">
-
-							
-								{#each item.tags as tag}
-
-
-								<li class="tag_icon">
-									<img alt="{tag.tag_name}"
-										src="//strapi.adamkarski.art/icons/{tag.tag_name}.svg"
-										class=" h-10 w-10 m-0 p-1 hover:bg-gray-100"
-									/>
-								</li>
-
-							
-
-							{/each}
-
-							
-								
-							</ul>
-							
-						</div>
-						
+		<div class=" box_ofer shadow-xl rounded-xl">
+			<div class=" content rounded-xl">
+				<div class=" backgr rounded-md" />
+				<div class=" texts rounded-md">
+					<div class=" p-3 title">
+						<h3 class=" font-semibold text-xl leading-6 my-2">{item.title}</h3>
+						<p class=" paragraph-normal opis">
+							{item.subtitle}
+						</p>
 					</div>
+				</div>
+				<div class=" flex items-center icons">
+					<div class="button_">
+						<a href="/realizacje/{item.slug}">
+							<button
+								class="bg-white text-black px-1 py-2 m-2 rounded-md pl-3"
+								style="transform: none;"
+							>
+								<img alt="" src={moreinfo} class="w10 h10" />
+							</button>
+						</a>
+					</div>
+
+					<ul class=" list-none flex">
+						{#each item.tags as tag}
+							<li class="tag_icon">
+								<img
+									alt={tag.tag_name}
+									src="//strapi.adamkarski.art/icons/{tag.tag_name}.svg"
+									class=" h-10 w-10 m-0 p-1 hover:bg-gray-100"
+								/>
+							</li>
+						{/each}
+					</ul>
 				</div>
 			</div>
 		</div>
-		<!-- <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+	</div>
+</div>
+<!-- <br/><br/><br/><br/><br/><br/><br/><br/><br/>
 {tagCurrent} / {ls.tag_name} -->
